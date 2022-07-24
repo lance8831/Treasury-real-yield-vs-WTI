20158fad2a08169dfe8ad6b407aa66277ce6517a
lance8831 <95580523+lance8831@users.noreply.github.com>
2022-07-24 20:47:55 +0900

    Update WTI.py

--- WTI.py
+++ WTI.py
@@ -10,14 +10,14 @@ WTI = wb.DataReader("CL=F",data_source ="yahoo", start = "2003-1-1")["Adj Close"
 WTI = WTI.drop(["2002-12-31"])
 
 #Convert imported data into csv
-filepath = Path("C:/Users/Zhen Hao/Downloads/PY4E/Practice/WTI_price.csv")
+filepath = Path("C:/Files/WTI_price.csv")
 filepath.parent.mkdir(parents=True, exist_ok=True)
 WTI.to_csv(filepath)
 
 #rename column from adj close to price
-mod_WTI = pd.read_csv("C:/Users/Zhen Hao/Downloads/PY4E/Practice/WTI_price.csv")
+mod_WTI = pd.read_csv("C:/Files/WTI_price.csv")
 mod_WTI = mod_WTI.rename(columns={"Date":"Date_WTI","Adj Close":"WTI_Closing_Price"})
-mod_WTI.to_csv("C:/Users/Zhen Hao/Downloads/PY4E/Practice/WTI_price.csv",index=None)
+mod_WTI.to_csv("C:/Files/WTI_price.csv",index=None)
 check_info = mod_WTI.info()
 
 print("Data downloaded as CSV files")
