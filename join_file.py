5365a055978919929230d7bab2aaf1e432b5c6ed
lance8831 <95580523+lance8831@users.noreply.github.com>
2022-07-24 20:46:24 +0900

    Update join_file.py

--- join_file.py
+++ join_file.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pathlib import Path
 
 #setting path for joining multiple files
-files = os.path.join("C:/Users/Zhen Hao/Downloads/PY4E/Practice/Treasury_real_yield_yearly", "treasury_real_yield*.csv")
+files = os.path.join("C:/Files/Treasury_real_yield_yearly", "treasury_real_yield*.csv")
 
 #merged file returned
 files = glob.glob(files)
@@ -14,7 +14,7 @@ new_csv = pd.concat(map(pd.read_csv, files), ignore_index=True)
 new_csv = new_csv.fillna("N/A")
 
 #Export the file into CSV format
-filepath = Path("C:/Users/Zhen Hao/Downloads/PY4E/Practice/Treasury_real_yield_yearly/treasury_real_yield_all.csv")
+filepath = Path("C:/Files/treasury_real_yield_all.csv")
 filepath.parent.mkdir(parents=True, exist_ok=True)
 new_csv.to_csv(filepath,index=None)
 
